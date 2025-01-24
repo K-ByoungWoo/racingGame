f1f5daccf1e1d41f02a5b42e0d309abdac54a89e
SIRU <rkdquddn584@navere.com>
2025-01-24 15:52:31 +0900

    테스트 환경 개발-1

---
+++ Assets/Scripts/Calculator/Calculator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour
+{
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+}
